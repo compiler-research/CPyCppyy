c6780641bc2050696f2ddc8721058f43b0ce36fd
Aaron Jomy <aaronjomyjoseph@gmail.com>
2025-02-07 19:40:17 -0800

    Fully scope C++ names of enum values
    Based on https://github.com/wlav/CPyCppyy/commit/cb49e9b8fc37294dd6b42635d6dbbb4f2e565029

--- src/CPPEnum.cxx
+++ src/CPPEnum.cxx
@@ -209,10 +209,13 @@ CPyCppyy::CPPEnum* CPyCppyy::CPPEnum_New(const std::string& name, Cppyy::TCppSco
                 values_ok = false;
                 break;
             }
-            PyObject* pydname = CPyCppyy_PyText_FromString(Cppyy::GetFinalName(econstant).c_str());
+            const std::string& dname = Cppyy::GetFinalName(econstant);
+            PyObject* pydname = CPyCppyy_PyText_FromString(dname.c_str());
             PyObject_SetAttr(pyenum, pydname, val);
-            PyObject_SetAttr(val, PyStrings::gCppName, pydname);
             Py_DECREF(pydname);
+            PyObject* pydcppname = CPyCppyy_PyText_FromString((ename.empty() ? dname : (ename+"::"+dname)).c_str());
+            PyObject_SetAttr(val, PyStrings::gCppName, pydcppname);
+            Py_DECREF(pydcppname);
             Py_DECREF(val);
         }
 
