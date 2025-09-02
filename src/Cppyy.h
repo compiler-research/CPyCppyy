299ca689ef488bfc1e2ce4f29a9c61fe29bff846
Vipul Cariappa <vipulcariappa@gmail.com>
2025-09-02 15:03:03 +0200

    impl namespaces, templated functions & classes

--- src/Cppyy.h
+++ src/Cppyy.h
@@ -196,6 +196,8 @@ namespace Cppyy {
     CPPYY_IMPORT
     bool IsMethod(TCppScope_t handle);
     CPPYY_IMPORT
+    bool IsTemplateClass(TCppScope_t handle);
+    CPPYY_IMPORT
     bool IsTemplate(TCppScope_t handle);
     CPPYY_IMPORT
     bool IsTemplateInstantiation(TCppScope_t handle);
@@ -277,9 +279,13 @@ namespace Cppyy {
     CPPYY_IMPORT
     std::string GetMethodReturnTypeAsString(TCppMethod_t);
     CPPYY_IMPORT
+    TCppIndex_t GetTemplateNumArgs(TCppScope_t);;
+    CPPYY_IMPORT
     TCppIndex_t GetMethodNumArgs(TCppMethod_t);
     CPPYY_IMPORT
     TCppIndex_t GetMethodReqArgs(TCppMethod_t);
+    CPPYY_IMPORT
+    std::string GetTemplateArgName(TCppMethod_t, TCppIndex_t iarg);
     CPPYY_IMPORT
     std::string GetMethodArgName(TCppMethod_t, TCppIndex_t iarg);
     CPPYY_IMPORT
@@ -306,6 +312,8 @@ namespace Cppyy {
     std::string GetTemplatedMethodName(TCppScope_t scope, TCppIndex_t imeth);
     CPPYY_IMPORT
     bool        ExistsMethodTemplate(TCppScope_t scope, const std::string& name);
+    CPPYY_IMPORT
+    bool        IsPureTemplatedMethod(TCppMethod_t method);
     CPPYY_IMPORT
     bool        IsTemplatedMethod(TCppMethod_t method);
     CPPYY_IMPORT
@@ -347,6 +355,8 @@ namespace Cppyy {
     TCppScope_t WrapLambdaFromVariable(TCppScope_t var);
     CPPYY_IMPORT
     TCppScope_t AdaptFunctionForLambdaReturn(TCppScope_t fn);
+    CPPYY_IMPORT
+    void GetMemberInNamespace(TCppScope_t ns, std::vector<TCppScope_t>& members);
     CPPYY_IMPORT
     TCppType_t  GetDatamemberType(TCppScope_t var);
     CPPYY_IMPORT
--- src/TypeHints.cxx
+++ src/TypeHints.cxx
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <sstream>
+#include <fstream>
 #include <algorithm>
 
 inline std::string replace_all(std::string str, const std::string& from, const std::string& to) {
@@ -92,6 +93,13 @@ std::string pythonize_type_name(std::string cpptype) {
 }
 
 // TODO: extract docs from C++ code
+void handle_scope(Cppyy::TCppScope_t typ, std::ostringstream &file, int indent = 0);
+void handle_namespace(Cppyy::TCppScope_t ns, std::ostringstream &file, int indent = 0);
+void handle_class(Cppyy::TCppScope_t cls, std::ostringstream &file, int indent = 0);
+void handle_templates(Cppyy::TCppScope_t tmpl, std::ostringstream &file, int indent = 0);
+void handle_function(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent = 0);
+void handle_typedef(Cppyy::TCppScope_t var, std::ostringstream &file, int indent = 0);
+void handle_variable(Cppyy::TCppScope_t var, std::ostringstream &file, int indent);
 
 void handle_variable(Cppyy::TCppScope_t var, std::ostringstream &file, int indent = 0) {
     std::string indentation_str = indentation(indent);
@@ -103,7 +111,7 @@ void handle_variable(Cppyy::TCppScope_t var, std::ostringstream &file, int inden
         << "\n";
 }
 
-void handle_typedef(Cppyy::TCppScope_t var, std::ostringstream &file, int indent = 0) {
+void handle_typedef(Cppyy::TCppScope_t var, std::ostringstream &file, int indent) {
     std::string indentation_str = indentation(indent);
 
     file << indentation_str
@@ -113,7 +121,7 @@ void handle_typedef(Cppyy::TCppScope_t var, std::ostringstream &file, int indent
         << "\n";
 }
 
-void handle_function(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent = 0) {
+void handle_function(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent) {
     static int A = 0;
     std::string indentation_str = indentation(indent);
 
@@ -144,8 +152,10 @@ void handle_function(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent
         prepend_comma = true;
     }
     for (size_t i = 0, nArgs = Cppyy::GetMethodNumArgs(fn); i < nArgs; i++) {
-        if (prepend_comma)
+        if (prepend_comma) {
             file << ", ";
+            prepend_comma = false;
+        }
         std::string arg_name = Cppyy::GetMethodArgName(fn, i);
         file << (arg_name.empty() ? ("_" + std::to_string(++A)) : arg_name)
             << ": "
@@ -161,7 +171,144 @@ void handle_function(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent
         << ": ...\n";
 }
 
-void handle_templates(Cppyy::TCppScope_t fn, std::ostringstream &file, int indent = 0) { exit(1); }
+void handle_templates(Cppyy::TCppScope_t tmpl, std::ostringstream &file, int indent) { 
+    static int A = 0;
+    std::string indentation_str = indentation(indent);
+
+    // TODO: handle parameter pack
+    // TODO: handle defaults
+    
+    if (Cppyy::IsPureTemplatedMethod(tmpl)) {
+        std::string func_name = Cppyy::GetFinalName(tmpl);
+        std::string class_name;
+        Cppyy::TCppScope_t parent = Cppyy::GetParentScope(tmpl);
+        bool is_method = false;
+        if (Cppyy::IsClass(parent)) {
+            class_name = Cppyy::GetMethodName(parent);
+            func_name = pythonize_method_name(func_name, class_name);
+            is_method = true;
+        }
+        if (func_name.empty())
+            return; // this is a overload that cannot be represented in python
+        file << indentation_str
+            << "@overload\n";
+        if (Cppyy::IsStaticMethod(tmpl)) {
+            file << indentation_str
+                << "@staticmethod\n";
+        }
+        file << indentation_str
+            << "def "
+            << func_name
+            << "[";
+        // write template args
+        for (size_t i = 0, nArgs = Cppyy::GetTemplateNumArgs(tmpl); i < nArgs; i++) {
+            if (i != 0)
+                file << ", ";
+            std::string arg_name = Cppyy::GetTemplateArgName(tmpl, i);
+            file << (arg_name.empty() ? ("_" + std::to_string(++A)) : arg_name);
+        }
+        file << "](";
+        bool prepend_comma = false;
+        if (is_method && !Cppyy::IsStaticMethod(tmpl)) {
+            // add the implicit self argument
+            file << "self: "
+                << pythonize_type_name(class_name);
+            prepend_comma = true;
+        }
+        for (size_t i = 0, nArgs = Cppyy::GetMethodNumArgs(tmpl); i < nArgs; i++) {
+            if (prepend_comma) {
+                file << ", ";
+                prepend_comma = false;
+            }
+            std::string arg_name = Cppyy::GetMethodArgName(tmpl, i);
+            file << (arg_name.empty() ? ("_" + std::to_string(++A)) : arg_name)
+                << ": "
+                << pythonize_type_name(Cppyy::GetMethodArgTypeAsString(tmpl, i));
+            std::string default_value = Cppyy::GetMethodArgDefault(tmpl, i); // FIXME: Pythonize this value
+            if (!default_value.empty())
+                file << " = " << default_value;
+            if (i != nArgs - 1)
+                file << ", ";
+        }
+        file << ") -> "
+            << pythonize_type_name(Cppyy::GetMethodReturnTypeAsString(tmpl))
+            << ": ...\n";
+    } else {
+        // is templated class
+    }
+}
+
+void handle_class(Cppyy::TCppScope_t cls, std::ostringstream &file, int indent) { 
+    static size_t A = 0;
+    std::string indentation_str = indentation(indent);
+
+    bool is_templated = Cppyy::IsTemplateClass(cls);
+    // TODO: handle inheritance
+    file << indentation_str
+        << "class "
+        << Cppyy::GetMethodName(cls);
+    if (is_templated) {
+        file << "[";
+        for (size_t i = 0, nArgs = Cppyy::GetTemplateNumArgs(cls); i < nArgs; i++) {
+            if (i != 0)
+                file << ", ";
+            std::string arg_name = Cppyy::GetTemplateArgName(cls, i);
+            file << (arg_name.empty() ? ("_" + std::to_string(++A)) : arg_name);
+        }
+        file << "]";
+    }
+    file << ":\n";
+    
+    std::vector<Cppyy::TCppScope_t> members;
+    Cppyy::GetDatamembers(cls, members);
+    for (Cppyy::TCppScope_t datamember: members)
+        handle_variable(datamember, file, indent + 1);
+    
+    members.clear();
+    Cppyy::GetClassMethods(cls, members);
+    file << "\n";
+    for (Cppyy::TCppScope_t datamember: members) {
+        file << "\n";
+        handle_function(datamember, file, indent + 1);
+    }
+    
+    members.clear();
+    Cppyy::GetTemplatedMethods(cls, members);
+    file << "\n";
+    for (Cppyy::TCppScope_t datamember: members) {
+        file << "\n";
+        handle_templates(datamember, file, indent + 1);
+    }
+    file << "\n\n";
+
+    // FIXME: typedefs, enums, & nexted classes missing
+}
+
+void handle_namespace(Cppyy::TCppScope_t ns, std::ostringstream &file, int indent) {
+    std::vector<Cppyy::TCppScope_t> members;
+    Cppyy::GetMemberInNamespace(ns, members);
+    file << "class " << Cppyy::GetMethodName(ns) << ":\n";
+    for (Cppyy::TCppScope_t s: members)
+        handle_scope(s, file, indent + 1);
+}
+
+void handle_scope(Cppyy::TCppScope_t typ, std::ostringstream &file, int indent) {
+    if (Cppyy::IsNamespace(typ)) {
+        handle_namespace(typ, file, indent);
+    }
+    if (Cppyy::IsClass(typ) || Cppyy::IsTemplateClass(typ)) {
+        handle_class(typ, file, indent);
+    }
+    if (Cppyy::IsTypedefed(typ)) {
+        handle_typedef(typ, file, indent);
+    }
+    if (Cppyy::IsFunction(typ) && !Cppyy::IsMethod(typ) && !Cppyy::IsPureNamespace(Cppyy::GetParentScope(typ))) {
+        handle_function(typ, file, indent);
+    }
+    if (Cppyy::IsVariable(typ) && !Cppyy::IsPureNamespace(Cppyy::GetParentScope(typ)) && !Cppyy::IsClass(Cppyy::GetParentScope(typ))) {
+        handle_variable(typ, file, indent);
+    }
+}
 
 PyObject *generate_typehints(PyObject *, PyObject *args) {
     const char *type_name = nullptr;
@@ -175,51 +322,17 @@ PyObject *generate_typehints(PyObject *, PyObject *args) {
         return PyErr_Format(PyExc_TypeError, "Unknown Type. Try Parent.");
 
     std::ostringstream code;
-    if (Cppyy::IsNamespace(typ)) {
-        return PyErr_Format(PyExc_NotImplementedError, "Type hint generation for C++ namespace not implemented");
-    }
-    if (Cppyy::IsClass(typ)) {
-        // TODO: handle inheritance
-        code << "class "
-            << Cppyy::GetMethodName(typ)
-            << ":\n";
-        
-        std::vector<Cppyy::TCppScope_t> members;
-        Cppyy::GetDatamembers(typ, members);
-        for (Cppyy::TCppScope_t datamember: members)
-            handle_variable(datamember, code, 1);
-        
-        members.clear();
-        Cppyy::GetClassMethods(typ, members);
-        code << "\n";
-        for (Cppyy::TCppScope_t datamember: members) {
-            code << "\n";
-            handle_function(datamember, code, 1);
-        }
-        
-        members.clear();
-        Cppyy::GetTemplatedMethods(typ, members);
-        code << "\n";
-        for (Cppyy::TCppScope_t datamember: members) {
-            code << "\n";
-            handle_templates(datamember, code, 1);
-        }
+    handle_scope(typ, code);
+    code << "\n";
 
-        // FIXME: typedefs, enums, & nexted classes missing
-    }
-    if (Cppyy::IsTemplate(typ)) {
-        return PyErr_Format(PyExc_NotImplementedError, "Type hint generation for C++ template not implemented");
-    }
-    if (Cppyy::IsTypedefed(typ)) {
-        handle_typedef(typ, code);
-    }
-    if (Cppyy::IsFunction(typ) && !Cppyy::IsMethod(typ) && !Cppyy::IsPureNamespace(Cppyy::GetParentScope(typ))) {
-        handle_function(typ, code);
-    }
-    if (Cppyy::IsVariable(typ) && !Cppyy::IsPureNamespace(Cppyy::GetParentScope(typ)) && !Cppyy::IsClass(Cppyy::GetParentScope(typ))) {
-        handle_variable(typ, code);
-    }
-    if (!code)
+    std::string typehint = code.str();
+    if (typehint.empty())
         return PyErr_Format(PyExc_TypeError, "Cannot generate typehints for the given type <%s>. Try Parent.", Cppyy::GetScopedFinalName(typ).c_str());
-    return PyUnicode_FromString(code.str().c_str());
+
+    if (!file_name)
+        return PyUnicode_FromString(typehint.c_str());
+
+    std::fstream file(file_name, std::ios::out | std::ios::ate); // append
+    file << typehint;
+    Py_RETURN_NONE;
 }
