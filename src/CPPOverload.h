c3c833ba8e169834adb45f2e4cd0474e57cb319f
Vipul Cariappa <vipulcariappa@gmail.com>
2025-11-28 14:18:45 +0100

    use CPPOverload's name in hashing for the TemplateProxy cache
    FIXME: Instantiate should resolve based on both templated and non-templated options

--- src/CPPOverload.h
+++ src/CPPOverload.h
@@ -13,31 +13,6 @@
 
 namespace CPyCppyy {
 
-// signature hashes are also used by TemplateProxy
-inline uint64_t HashSignature(CPyCppyy_PyArgs_t args, size_t nargsf)
-{
-// Build a hash from the types of the given python function arguments.
-    uint64_t hash = 0;
-
-    Py_ssize_t nargs = CPyCppyy_PyArgs_GET_SIZE(args, nargsf);
-    for (Py_ssize_t i = 0; i < nargs; ++i) {
-    // TODO: hashing in the ref-count is for moves; resolve this together with the
-    // improved overloads for implicit conversions
-        PyObject* pyobj = CPyCppyy_PyArgs_GET_ITEM(args, i);
-        hash += (uint64_t)Py_TYPE(pyobj);
-#if PY_VERSION_HEX >= 0x030e0000
-        hash += (uint64_t)(PyUnstable_Object_IsUniqueReferencedTemporary(pyobj) ? 1 : 0);
-#else
-        hash += (uint64_t)(Py_REFCNT(pyobj) == 1 ? 1 : 0);
-#endif
-        hash += (hash << 10); hash ^= (hash >> 6);
-    }
-
-    hash += (hash << 3); hash ^= (hash >> 11); hash += (hash << 15);
-
-    return hash;
-}
-
 class CPPOverload {
 public:
     typedef std::vector<std::pair<uint64_t, PyCallable*>> DispatchMap_t;
@@ -118,6 +93,38 @@ inline CPPOverload* CPPOverload_New(const std::string& name, PyCallable* method)
     std::vector<PyCallable*> p;
     p.push_back(method);
     return CPPOverload_New(name, p);
+}
+
+// signature hashes are also used by TemplateProxy
+inline uint64_t HashSignature(CPyCppyy_PyArgs_t args, size_t nargsf) {
+  // Build a hash from the types of the given python function arguments.
+  uint64_t hash = 0;
+
+  Py_ssize_t nargs = CPyCppyy_PyArgs_GET_SIZE(args, nargsf);
+  for (Py_ssize_t i = 0; i < nargs; ++i) {
+    // TODO: hashing in the ref-count is for moves; resolve this together with
+    // the improved overloads for implicit conversions
+    PyObject *pyobj = CPyCppyy_PyArgs_GET_ITEM(args, i);
+    hash += (uint64_t)Py_TYPE(pyobj);
+#if PY_VERSION_HEX >= 0x030e0000
+    hash += (uint64_t)(PyUnstable_Object_IsUniqueReferencedTemporary(pyobj) ? 1 : 0);
+#else
+    hash += (uint64_t)(Py_REFCNT(pyobj) == 1 ? 1 : 0);
+#endif
+    if (CPPOverload_CheckExact(pyobj)) {
+      static std::hash<std::string> hash_string;
+      CPPOverload *overload = (CPPOverload *)pyobj;
+      hash += hash_string(overload->fMethodInfo->fName);
+    }
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
+
+  return hash;
 }
 
 } // namespace CPyCppyy
